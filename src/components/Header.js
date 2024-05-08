8179b527a8710012c00ba4884f2565f7d55034e1
Karunlal <karunlalp@gmail.com>
2024-05-08 13:47:46 +0530

    bug fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../utils/firebase'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { addUser, removeUser } from '../utils/userSlice'
+import { onAuthStateChanged } from 'firebase/auth'
 
 const Header = () => {
+  const dispatch = useDispatch()
   const navigate = useNavigate()
-
+  const user = useSelector((store) => store.user)
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -18,6 +21,23 @@ const Header = () => {
         navigate('/error')
       })
   }
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName } = user
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }))
+        navigate('/browse')
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser())
+        navigate('/')
+      }
+    })
+  }, [])
   return (
     <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
