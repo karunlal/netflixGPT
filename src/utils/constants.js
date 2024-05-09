2ea99b9a7f603c0754bfa61273c571be1efd227e
Karunlal <karunlalp@gmail.com>
2024-05-09 19:29:38 +0530

    hooks started

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   'https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e'
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjZkZjFiNWQxMjAwMWJmNzY5NDJiZWUyZGY4Njk0OSIsInN1YiI6IjY2M2M2NjM2ZDEzYTMzNDFjZTc4NWZhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0bk5t2LlJohAZoE4R3S6W_iU_WiprMlZTVvsKyLtFfs',
+  },
+}
